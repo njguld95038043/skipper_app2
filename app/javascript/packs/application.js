647c43b107eb3a1d0a7237add1035593f599cd9f
njguld95038043 <t.tamura6060@gmail.com>
2022-01-25 02:26:34 +0000

    [Finish]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "script.js"
+import 'jquery'
+import "scripts/skippr.min"
 
 Rails.start()
 Turbolinks.start()
